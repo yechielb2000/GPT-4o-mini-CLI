fe6e8df4bee9c66c0d60dcc04fafc297271e06d9
Yechiel Babani <yechielb2000@gmail.com>
2025-08-24 18:54:04 +0300

    add extra capabilities

--- session/session.go
+++ session/session.go
@@ -1,19 +1,26 @@
 package session
 
+type Message struct {
+	sender  string
+	content string
+}
+
 type Session struct {
-	rcvMsg chan []byte
+	rcvMsgContent   chan []byte
+	messagesHistory []Message
 }
 
 func NewSessionConfig() *Session {
 	return &Session{
-		rcvMsg: make(chan []byte),
+		rcvMsgContent:   make(chan []byte),
+		messagesHistory: make([]Message, 0),
 	}
 }
 
 func (cfg *Session) SendMessage() {
 	// on user demand.
 	// send a request containing the message.
-	// the request returns and fills the rcvMsg channel...
+	// the request returns and fills the rcvMsgContent channel...
 }
 
 func (cfg *Session) ReceiveMessage() {
@@ -24,4 +31,12 @@ func (cfg *Session) ReceiveMessage() {
 
 func (cfg *Session) Close() {
 	// close session (the socket? we will see)
+}
+
+func (cfg *Session) ReturnToCLI() {
+	// return to cli without losing this session
+}
+
+func (cfg *Session) saveMessage(sender string, content string) {
+	cfg.messagesHistory = append(cfg.messagesHistory, Message{sender: sender, content: content})
 }
