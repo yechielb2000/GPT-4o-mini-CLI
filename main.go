f3d34c7769ad41882d64bfd92b817d429504bdde
Yechiel Babani <yechielb2000@gmail.com>
2025-08-24 16:58:31 +0300

    scketing commands

--- main.go
+++ main.go
@@ -7,6 +7,12 @@ I want to be able to
 - create a new session
 - return to one of the last sessions
 - get / update api key
+
+simulating commands
+new session add (session name)
+session (session name)
+api key -u "newapikey"
+api key (print new key)
 */
 
 func main() {
