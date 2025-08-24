2847cc050f39a6683bf5db62576c861a4832e3f0
Yechiel Babani <yechielb2000@gmail.com>
2025-08-24 16:54:29 +0300

    add comment

--- main.go
+++ main.go
@@ -2,6 +2,13 @@ package main
 
 import "gpt4omini/cmd"
 
+/*
+I want to be able to
+- create a new session
+- return to one of the last sessions
+- get / update api key
+*/
+
 func main() {
 	cmd.Execute()
 }
