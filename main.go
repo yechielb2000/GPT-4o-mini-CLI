6f196772bece4dd2c3de3c1a7920e112bf948d7b
Yechiel Babani <yechielb2000@gmail.com>
2025-08-27 13:33:34 +0300

    remove the unnseccesery init of the manager

--- main.go
+++ main.go
@@ -3,12 +3,10 @@ package main
 import (
 	"github.com/spf13/viper"
 	"gpt4omini/cmd"
-	"gpt4omini/session"
 )
 
 func main() {
 	viper.SetEnvPrefix(cmd.CliName)
 	viper.AutomaticEnv()
-	_ = session.GetSessionsManager()
 	cmd.Execute()
 }
