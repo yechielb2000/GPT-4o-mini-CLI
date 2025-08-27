deebf5a557dcac0a1b27c641925ee0431d1155ad
Yechiel Babani <yechielb2000@gmail.com>
2025-08-27 18:47:49 +0300

    refactoring sessions subcommand to really do what it should xd

--- cmd/session.go
+++ cmd/session.go
@@ -1,10 +1,8 @@
 package cmd
 
 import (
-	"fmt"
 	"github.com/spf13/cobra"
 	"gpt4omini/session"
-	"log"
 )
 
 const SessionName string = "session"
@@ -25,40 +23,11 @@ session "session name" return to session
 var sessionCmd = &cobra.Command{
 	Use:   SessionName,
 	Short: "Make session actions",
-	Long: fmt.Sprintf(
-		`Make session actions using this subcommand.
-For example: %s %s -n. This starts a new session`, CliName, SessionName,
-	),
+	Long:  "",
 	Run: func(cmd *cobra.Command, args []string) {
-		if showList {
-			for id := range sessionsManager.Sessions() {
-				fmt.Println("RealtimeSession ID:", id)
-			}
-		} else if startNewSession {
-			newSession, err := session.NewRealtimeSession()
-			if err != nil {
-				log.Fatal(err)
-			}
-			sessionsManager.AddSession(newSession)
-			if err != nil {
-				fmt.Println(err)
-				return
-			}
-			newSession.Start()
-		} else if sessionId != "" {
-			s, err := sessionsManager.GetSession(sessionId)
-			if err != nil {
-				log.Print(err)
-			}
-			s.Start()
-		}
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(sessionCmd)
-	sessionCmd.Flags().BoolVarP(&startNewSession, "new", "n", false, "Create new session.")
-	sessionCmd.Flags().BoolVarP(&showList, "list", "l", false, "Get last active sessions.")
-	sessionCmd.Flags().StringVarP(&sessionId, "get", "g", "", "Return to session if exists.")
-	sessionCmd.MarkFlagsMutuallyExclusive("new", "get", "list")
 }
