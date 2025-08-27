a2a2cd5a68a17fd5fa806028bba96ddec632ce95
Yechiel Babani <yechielb2000@gmail.com>
2025-08-27 13:11:35 +0300

    making configCmd temaplte

--- cmd/config.go
+++ cmd/config.go
@@ -1,1 +1,14 @@
 package cmd
+
+import "github.com/spf13/cobra"
+
+const ConfigCmd = "config"
+
+var configCmd = &cobra.Command{
+	Use:   ConfigCmd,
+	Short: "View or update config file",
+	Long: `Manage the configuration stored in config.yaml.
+You can print the current config or update API/model fields.`,
+}
+
+func init() {}
