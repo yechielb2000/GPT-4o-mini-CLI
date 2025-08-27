bf8fd1eae79090264f826838cd8c4456b846cd3e
Yechiel Babani <yechielb2000@gmail.com>
2025-08-27 15:17:36 +0300

    making the types more organized

---
+++ types/content.go
@@ -0,0 +1,27 @@
+package types
+
+/*
+Source: https://github.com/openai/openai-python/blob/main/src/openai/types/
+There is no exact definition for content. it's just a TypeAlias for all the defined content types.
+We declare an interface for the content and then make content types out of it.
+*/
+
+// Content The interface for all content types
+type Content interface {
+	ContentType() string
+}
+
+// BaseContent shares params across all content types
+type BaseContent struct {
+	// Type The type of the input item.
+	Type string `json:"type"`
+}
+
+func (b BaseContent) ContentType() string {
+	return b.Type
+}
+
+type TextContent struct {
+	BaseContent
+	Text string `json:"text"`
+}
